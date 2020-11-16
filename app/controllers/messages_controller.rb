550d6ea88673706aa20c9467f4fe71ebc21c3d12
Insight-Creator <luizbuchhalter@Outlook.com>
2020-11-16 16:46:29 -0300

    Add Channel Controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,11 @@
+class MessagesController < ApplicationController
+  @chatroom = Chatroom.find(params[:chatroom_id])
+  @message = Message.new(message_params)
+  @message.chatroom = @chatroom
+  @message.user = current_user
+  if @message.save
+    redirect_to chatroom_path(@chatroom, achor: "message-#{@message.id}")
+  else
+    render "chatrooms/show"
+  end
+end
