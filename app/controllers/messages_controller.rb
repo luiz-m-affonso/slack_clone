30dbca4252cfbca8bf85b8f18291505a6283913d
Insight-Creator <luizbuchhalter@Outlook.com>
2020-11-16 16:57:15 -0300

    Updated Action Cable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,11 @@ class MessagesController < ApplicationController
   @message.chatroom = @chatroom
   @message.user = current_user
   if @message.save
+    # app/controllers/messages_controller.rb
+    ChatroomChannel.broadcast_to(
+      @chatroom,
+      render_to_string(partial: "message", locals: { message: @message })
+    )
     redirect_to chatroom_path(@chatroom, achor: "message-#{@message.id}")
   else
     render "chatrooms/show"
