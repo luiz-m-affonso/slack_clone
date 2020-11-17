0d8152f272713e52fc249a7708d6258ea249bbfe
Insight-Creator <luizbuchhalter@Outlook.com>
2020-11-17 15:28:32 -0300

    Updated Slack Clone

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,16 +1,20 @@
 class MessagesController < ApplicationController
-  @chatroom = Chatroom.find(params[:chatroom_id])
-  @message = Message.new(message_params)
-  @message.chatroom = @chatroom
-  @message.user = current_user
-  if @message.save
-    # app/controllers/messages_controller.rb
-    ChatroomChannel.broadcast_to(
-      @chatroom,
-      render_to_string(partial: "message", locals: { message: @message })
-    )
-    redirect_to chatroom_path(@chatroom, achor: "message-#{@message.id}")
-  else
-    render "chatrooms/show"
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(message_params)
+    @message.chatroom = @chatroom
+    @message.user = current_user
+    if @message.save
+      ChatroomChannel.broadcast_to(@chatroom, render_to_string(partial: "message", locals: { message: @message }))
+      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+    else
+      render "chatrooms/show"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Message.destroy_all
+Chatroom.destroy_all
+User.destroy_all
+
+boris = User.create(email: "boris@lewagon.org", password: "488F9MZ)*9QvvP7V26U?3b}vm{j{we7j", nickname: "Papillard")
+seb = User.create(email: "seb@lewagon.org", password: "?d8T97As349*8Cgpj&i8g3cCCs6;Xa", nickname: "ssaunier")
+
+general = Chatroom.create(name: "general")
+
+Message.create(chatroom: general, user: seb, content: "Hello all :wave:")
+Message.create(chatroom: general, user: boris, content: "Hello Seb :wave:")
+Message.create(chatroom: general, user: boris, content: "Action Cable rocks :rocket:")
+Message.create(chatroom: general, user: seb, content: "It seems we have N+1 queries though, let's fix that :ok_hand:")
